2a9f213013f82667775d72b09c18af087a5c0e00
Maciej Zubala <maciej.zubala@gmail.com>
2017-09-12 15:11:32 +0200

    pagination

--- src/main/java/pl/com/bottega/sales/infrastructure/read/JPAOrderBrowser.java
+++ src/main/java/pl/com/bottega/sales/infrastructure/read/JPAOrderBrowser.java
@@ -14,6 +14,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -29,10 +30,31 @@ public class JPAOrderBrowser implements OrderBrowser {
         CriteriaQuery criteriaQuery = criteriaBuilder.createQuery();
         Root order = criteriaQuery.from(Order.class);
         criteriaQuery.select(criteriaBuilder.construct(OrderDto.class, order.get("placedAt"), order.get("total")));
-        criteriaQuery.where(criteriaBuilder.equal(order.get("customer").get("customerId"), customerId));
+        criteriaQuery.orderBy(criteriaBuilder.desc(order.get("id")));
+        prepareQuery(customerId, criteriaBuilder, criteriaQuery, order);
+
+        CriteriaQuery countQuery = criteriaBuilder.createQuery();
+        Root countRoot = countQuery.from(Order.class);
+        countQuery.select(criteriaBuilder.count(countRoot));
+        prepareQuery(customerId, criteriaBuilder, countQuery, countRoot);
+
         Query query = entityManager.createQuery(criteriaQuery);
-        List<OrderDto> orderDtos = query.getResultList();
-        return new SearchResults<>(orderDtos, 0, 0, 0, 0);
+        List<OrderDto> orderDtos = query.
+                setFirstResult((pageNumber - 1) * pageSize).
+                setMaxResults(pageSize).
+                getResultList();
+
+        long total = (long) entityManager.createQuery(countQuery).getSingleResult();
+
+        return new SearchResults<OrderDto>(
+                orderDtos,
+                (int) (total / pageSize + (total % pageSize > 0 ? 1 : 0)),
+                pageNumber, pageSize, total);
+    }
+
+    private void prepareQuery(Long customerId, CriteriaBuilder criteriaBuilder, CriteriaQuery criteriaQuery, Root root) {
+        Predicate predicate = criteriaBuilder.equal(root.get("customer").get("customerId"), customerId);
+        criteriaQuery.where(predicate);
     }
 
     @Override
@@ -40,7 +62,7 @@ public class JPAOrderBrowser implements OrderBrowser {
     public byte[] exportOrders(Long customerId) {
         List<Order> orders = entityManager.createQuery("FROM Order o LEFT JOIN FETCH o.orderItems").getResultList();
         OrdersXmlBuilder builder = new OrdersXmlBuilder();
-        for(Order order : orders) {
+        for (Order order : orders) {
             order.export(builder);
         }
         return builder.getProduct();
