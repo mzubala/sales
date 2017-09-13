df7381e380e447dcad11eed177f88efcc63794c8
Maciej Zubala <maciej.zubala@gmail.com>
2017-09-13 09:49:35 +0200

    query cache

--- src/test/java/pl/com/bottega/play/CacheTest.java
+++ src/test/java/pl/com/bottega/play/CacheTest.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.support.TransactionTemplate;
+import pl.com.bottega.common.domain.Money;
 import pl.com.bottega.sales.application.PurchaseProcess;
 import pl.com.bottega.sales.domain.Customer;
+import pl.com.bottega.sales.domain.Product;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -90,6 +92,102 @@ public class CacheTest {
         assertThat(stats.getSecondLevelCacheHitCount()).isEqualTo(1L);
         assertThat(stats.getSecondLevelCacheMissCount()).isEqualTo(2L);
         assertThat(stats.getSecondLevelCachePutCount()).isEqualTo(2L);
+    }
+
+    @Test
+    public void testQueryCache() {
+        inertProducts();
+
+        runInTransaction(() -> {
+
+            SessionFactory sessionFactory = em.getEntityManagerFactory().unwrap(SessionFactory.class);
+            Statistics stats = sessionFactory.getStatistics();
+            stats.setStatisticsEnabled(true);
+
+            Session session = em.unwrap(Session.class);
+            session.createQuery("FROM Product p WHERE p.onSale = true").
+                    setCacheable(true).
+                    list();
+
+            assertThat(stats.getQueryCachePutCount()).isEqualTo(1L);
+            assertThat(stats.getQueryCacheMissCount()).isEqualTo(1L);
+            assertThat(stats.getSecondLevelCachePutCount()).isEqualTo(50L);
+        });
+
+        runInTransaction(() -> {
+
+            SessionFactory sessionFactory = em.getEntityManagerFactory().unwrap(SessionFactory.class);
+            Statistics stats = sessionFactory.getStatistics();
+            stats.setStatisticsEnabled(true);
+
+            Session session = em.unwrap(Session.class);
+            session.createQuery("FROM Product p WHERE p.onSale = true").
+                    setCacheable(true).
+                    list();
+
+            assertThat(stats.getQueryCachePutCount()).isEqualTo(1L);
+            assertThat(stats.getQueryCacheMissCount()).isEqualTo(1L);
+            assertThat(stats.getSecondLevelCachePutCount()).isEqualTo(50L);
+
+            assertThat(stats.getQueryCacheHitCount()).isEqualTo(1L);
+            assertThat(stats.getSecondLevelCacheHitCount()).isEqualTo(50L);
+        });
+    }
+
+    @Test
+    public void testQueryCacheClear() {
+        inertProducts();
+
+        runInTransaction(() -> {
+
+            SessionFactory sessionFactory = em.getEntityManagerFactory().unwrap(SessionFactory.class);
+            Statistics stats = sessionFactory.getStatistics();
+            stats.setStatisticsEnabled(true);
+
+            Session session = em.unwrap(Session.class);
+            session.createQuery("FROM Product p WHERE p.onSale = true").
+                    setCacheable(true).
+                    setCacheRegion("onSaleProducts").
+                    list();
+
+            assertThat(stats.getQueryCachePutCount()).isEqualTo(1L);
+            assertThat(stats.getQueryCacheMissCount()).isEqualTo(1L);
+            assertThat(stats.getSecondLevelCachePutCount()).isEqualTo(50L);
+        });
+
+        runInTransaction(() -> {
+
+            SessionFactory sessionFactory = em.getEntityManagerFactory().unwrap(SessionFactory.class);
+            Statistics stats = sessionFactory.getStatistics();
+            stats.setStatisticsEnabled(true);
+
+            sessionFactory.getCache().evictQueryRegion("onSaleProducts");
+
+            Session session = em.unwrap(Session.class);
+            session.createQuery("FROM Product p WHERE p.onSale = true").
+                    setCacheable(true).
+                    setCacheRegion("onSaleProducts").
+                    list();
+
+            assertThat(stats.getQueryCachePutCount()).isEqualTo(2L);
+            assertThat(stats.getQueryCacheMissCount()).isEqualTo(2L);
+            assertThat(stats.getSecondLevelCachePutCount()).isEqualTo(50L);
+
+            assertThat(stats.getQueryCacheHitCount()).isEqualTo(0L);
+            assertThat(stats.getSecondLevelCacheHitCount()).isEqualTo(0L);
+        });
+    }
+
+    private void inertProducts() {
+        for (int i = 0; i < 100; i++) {
+            int finalI = i;
+            runInTransaction(() -> {
+                Product p = new Product("P " + finalI, new Money(finalI + 100));
+                if (finalI % 2 == 0)
+                    p.putOnSale();
+                em.persist(p);
+            });
+        }
     }
 
     private void runInTransaction(Runnable r) {
