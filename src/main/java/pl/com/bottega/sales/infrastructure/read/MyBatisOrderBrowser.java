09102ea9fad9988924422045e76ff051e716ed9a
Maciej Zubala <maciej.zubala@gmail.com>
2017-09-13 12:13:34 +0200

    my batis order browser

--- src/main/java/pl/com/bottega/sales/infrastructure/read/MyBatisOrderBrowser.java
+++ src/main/java/pl/com/bottega/sales/infrastructure/read/MyBatisOrderBrowser.java
@@ -1,5 +1,6 @@
 package pl.com.bottega.sales.infrastructure.read;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.com.bottega.sales.read.OrderBrowser;
 import pl.com.bottega.sales.read.OrderDto;
@@ -10,10 +11,15 @@ import java.util.List;
 @Component
 public class MyBatisOrderBrowser implements OrderBrowser {
 
+    @Autowired
+    private MyBatisOrderMapper mapper;
+
     @Override
     public SearchResults<OrderDto> browse(Long customerId, int pageNumber, int pageSize) {
-
-        return null;
+        Long total = mapper.totalCount(customerId);
+        long pagesCount = (total / pageSize + (total % pageSize > 0 ? 1 : 0));
+        List<OrderDto> page = mapper.orders(customerId, (pageNumber - 1) * pageSize, pageSize);
+        return new SearchResults<OrderDto>(page, (int) pagesCount, pageNumber, pageSize, total);
     }
 
     @Override
--- src/main/java/pl/com/bottega/sales/read/OrderDto.java
+++ src/main/java/pl/com/bottega/sales/read/OrderDto.java
@@ -1,18 +1,24 @@
 package pl.com.bottega.sales.read;
 
 import pl.com.bottega.common.domain.Money;
+import pl.com.bottega.sales.domain.Order;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class OrderDto {
 
     private LocalDate placedAt;
     private Money total;
+    private BigDecimal totalValue;
+    private String totalCurrency;
 
     public OrderDto(LocalDate placedAt, Money total) {
         this.placedAt = placedAt;
         this.total = total;
     }
+
+    OrderDto() {}
 
     public LocalDate getPlacedAt() {
         return placedAt;
@@ -28,5 +34,13 @@ public class OrderDto {
 
     public void setTotal(Money total) {
         this.total = total;
+    }
+
+    public void setTotalValue(BigDecimal totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public void setTotalCurrency(String totalCurrency) {
+        this.totalCurrency = totalCurrency;
     }
 }
