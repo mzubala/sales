05dfebf625797029a9f076c605d4cb5420dca183
Maciej Zubala <maciej.zubala@gmail.com>
2017-09-13 11:08:21 +0200

    my batis order browser

--- src/main/java/pl/com/bottega/sales/infrastructure/read/JPAOrderBrowser.java
+++ src/main/java/pl/com/bottega/sales/infrastructure/read/JPAOrderBrowser.java
@@ -18,7 +18,6 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
-@Component
 public class JPAOrderBrowser implements OrderBrowser {
 
     @PersistenceContext
--- src/main/java/pl/com/bottega/sales/infrastructure/read/MyBatisOrderBrowser.java
+++ src/main/java/pl/com/bottega/sales/infrastructure/read/MyBatisOrderBrowser.java
@@ -1,15 +1,18 @@
 package pl.com.bottega.sales.infrastructure.read;
 
+import org.springframework.stereotype.Component;
 import pl.com.bottega.sales.read.OrderBrowser;
 import pl.com.bottega.sales.read.OrderDto;
 import pl.com.bottega.sales.read.SearchResults;
 
 import java.util.List;
 
+@Component
 public class MyBatisOrderBrowser implements OrderBrowser {
 
     @Override
     public SearchResults<OrderDto> browse(Long customerId, int pageNumber, int pageSize) {
+
         return null;
     }
 
