8c6c966f84c81b908397b1d058aea9285233fa5e
Maciej Zubala <maciej.zubala@gmail.com>
2017-11-08 15:53:03 +0100

    Revert "my batis order browser"
    This reverts commit 05dfebf625797029a9f076c605d4cb5420dca183.

--- src/main/java/pl/com/bottega/sales/infrastructure/read/JPAOrderBrowser.java
+++ src/main/java/pl/com/bottega/sales/infrastructure/read/JPAOrderBrowser.java
@@ -18,6 +18,7 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
+@Component
 public class JPAOrderBrowser implements OrderBrowser {
 
     @PersistenceContext
--- src/main/java/pl/com/bottega/sales/infrastructure/read/MyBatisOrderBrowser.java
+++ src/main/java/pl/com/bottega/sales/infrastructure/read/MyBatisOrderBrowser.java
@@ -1,18 +1,15 @@
 package pl.com.bottega.sales.infrastructure.read;
 
-import org.springframework.stereotype.Component;
 import pl.com.bottega.sales.read.OrderBrowser;
 import pl.com.bottega.sales.read.OrderDto;
 import pl.com.bottega.sales.read.SearchResults;
 
 import java.util.List;
 
-@Component
 public class MyBatisOrderBrowser implements OrderBrowser {
 
     @Override
     public SearchResults<OrderDto> browse(Long customerId, int pageNumber, int pageSize) {
-
         return null;
     }
 
