9a9fcee9790ae495c086c53c00ad046028937f32
Maciej Zubala <maciej.zubala@gmail.com>
2017-11-08 15:52:35 +0100

    Revert "my batis order browser"
    This reverts commit 09102ea9fad9988924422045e76ff051e716ed9a.

--- src/main/java/pl/com/bottega/sales/infrastructure/read/MyBatisOrderBrowser.java
+++ src/main/java/pl/com/bottega/sales/infrastructure/read/MyBatisOrderBrowser.java
@@ -1,6 +1,5 @@
 package pl.com.bottega.sales.infrastructure.read;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.com.bottega.sales.read.OrderBrowser;
 import pl.com.bottega.sales.read.OrderDto;
@@ -11,15 +10,10 @@ import java.util.List;
 @Component
 public class MyBatisOrderBrowser implements OrderBrowser {
 
-    @Autowired
-    private MyBatisOrderMapper mapper;
-
     @Override
     public SearchResults<OrderDto> browse(Long customerId, int pageNumber, int pageSize) {
-        Long total = mapper.totalCount(customerId);
-        long pagesCount = (total / pageSize + (total % pageSize > 0 ? 1 : 0));
-        List<OrderDto> page = mapper.orders(customerId, (pageNumber - 1) * pageSize, pageSize);
-        return new SearchResults<OrderDto>(page, (int) pagesCount, pageNumber, pageSize, total);
+
+        return null;
     }
 
     @Override
